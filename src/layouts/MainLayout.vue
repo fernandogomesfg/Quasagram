ed4e3c4fdaae562df2d924122a5b82d3a6329256
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-07-30 12:38:11 -0700

    style: alteracoes do cabecalho

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,22 +1,18 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header
+      class="bg-white text-grey-10"
+      bordered
+    >
+
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
+        <q-toolbar-title
+          class="text-grand-hotel text-bold"
+        >
+          Quasagram
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
+
     </q-header>
 
     <q-footer
@@ -61,6 +57,10 @@ export default defineComponent({
 </script>
 
 <style lang="sass">
+  .q-toolbar__title
+    text-align: center
+    font-size: 30px
+
   q.footer
     q.tab__icon
       font-size: 30px
