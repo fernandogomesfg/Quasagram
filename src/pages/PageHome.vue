89dc493da7501e9bea61ce10926541be85a3dc1e
Fernando Gomes <fernandogomesbernardo@gmail.com>
2022-07-30 19:39:26 -0700

    style: implementacao do layout dos post

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,13 +1,51 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Home Page</h5>
+  <q-page class="constrain q-pa-md">
+    <q-card
+      class="card-post"
+      flat
+      bordered
+    >
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://github.com/fernandogomesfg.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">fernandogomesfg</q-item-label>
+          <q-item-label caption>
+            Maputo, Mozambique
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+
+      <q-img
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+      />
+
+      <q-card-section>
+        <div>Ponte Maputo</div>
+        <div class="text-caption text-grey">June 8, 9:24AM </div>
+      </q-card-section>
+
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+//import { defineComponent } from 'vue'
 
-export default defineComponent({
+export default {
   name: 'PageHome'
-})
+}
 </script>
+
+
+<style lang="sass">
+  .card-post
+    .q-img
+      min-height: 200px
+</style>
